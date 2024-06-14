5b74900fa883ad08884a69ad993be118d6fef829
Victor Delrio <victordelrio1411@gmail.com>
2024-06-13 23:00:56 -0600

    Update file

--- Opciones_click.cy.js
+++ Opciones_click.cy.js
@@ -13,6 +13,17 @@ describe("OPCIONES DE CLICK", () => {
         cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type("admin123")
         cy.get('.oxd-button').should("be.visible").click()
     })
+//Se usa cuando hay elementos que interfieren con un boton
+    it.only("Click Force true", () => {
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        cy.title().should('eq', 'OrangeHRM')
+        cy.wait(1000)
+
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type("Admin")
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type("admin123")
+        cy.get('.oxd-button').should("be.visible").click({force: true})
+    })
+
 
 
 
